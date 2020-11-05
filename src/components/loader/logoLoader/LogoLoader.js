c325f2e401fbe97ac4d72d391b868ea0616434d6
bensonodede <bensonodede@gmail.com>
2020-11-05 22:28:46 +0300

    Logo loader margin/indent style fix

--- src/components/loader/logoLoader/LogoLoader.js
+++ src/components/loader/logoLoader/LogoLoader.js
@@ -5,9 +5,11 @@ import "./styles.scss";
 
 const LogoLoader = () => (
   <div className="logo-loader">
-    <h5 className="title is-size-7 has-text-centered logo-loader__title">
-      {process.env.GATSBY_STORE_NAME}
-    </h5>
+    <div className="logo-loader__title-wrapper">
+      <h5 className="title is-size-7 has-text-centered logo-loader__title">
+        {process.env.GATSBY_STORE_NAME}
+      </h5>
+    </div>
   </div>
 );
 
